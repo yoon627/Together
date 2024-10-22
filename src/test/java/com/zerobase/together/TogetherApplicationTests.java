dc0c2da22a40b6a1a13c926558f49c4864cda1ad
yoon627 <science627@naver.com>
2024-10-22 17:50:28 +0900

    Initial commit

---
+++ src/test/java/com/zerobase/together/TogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
